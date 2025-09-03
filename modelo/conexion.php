6c713d43d80a12f084c92060061f0b8b79ffd165
rodzharick <juansantan1@colegioguanenta.edu.co>
2025-09-03 17:29:56 -0500

    se actualizo

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -38,7 +38,7 @@
 <head>
    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="../css/index.css">
 </head>
 <body>
     
--- modelo/login_supervisor.php
+++ modelo/login_supervisor.php
@@ -8,7 +8,7 @@ ini_set('display_errors','1');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="../css/index.css">
     <title>Login</title>
 </head>
 <body>
